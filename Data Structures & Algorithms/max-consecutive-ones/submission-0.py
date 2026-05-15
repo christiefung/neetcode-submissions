c8f5a9692935bb79be75114e4fa3af220449a871
christiefung <fkmchristie@gmail.com>
2026-05-15 16:15:16 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        current_streak = 0
+        max_streak = 0
+        for num in nums:
+            if num == 1:
+                current_streak += 1
+                
+                if current_streak > max_streak:
+                    max_streak = current_streak
+                
+            elif num == 0:           
+                current_streak = 0
+
+
+        return(max_streak)
